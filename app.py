580689094280aabd742340c8d17522f9ef475c5d
Natalia <natalia1andriychuk@gmail.com>
2019-02-09 19:23:11 -0500

    finalazing code

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from datetime import datetime
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite?check_same_thread=False")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -39,10 +39,24 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+@app.route("/")
+def home():
+    # Creating a landing page
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+    )
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    # Createing a query to retrieve dates and precipitation values
     scores = session.query(Measurement.date, Measurement.prcp).all()
     
+    # Converting the query results to a Dictionary
     precipitation = []
     for score in scores:
         precipitation_dict = {}
@@ -54,93 +68,69 @@ def precipitation():
 
 @app.route("/api/v1.0/stations")
 def stations():
-    stations = session.query(Station.station).all()
-    
+    # Createing a query to retrieve  station values
+    stations_query = session.query(Station.station).all()
+
+    # Converting the query results to a Dictionary
+    stations = []
+    for station_val in stations_query:
+        stations_dict = {}
+        stations_dict["station"] = station_val.station
+        stations.append(stations_dict)
     return jsonify(stations)
-#     return (
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/names<br/>"
-#         f"/api/v1.0/passengers"
-# #     )
+#     
 @app.route("/api/v1.0/tobs")
 def tobs():
+    # Calculate latest date in the database
     latest_date = session.query(Measurement.date).first()
-# Convert latest_date string to a date object
+    # Convert latest_date string to a date object
     date_object = datetime.strptime(str(latest_date), "('%Y-%m-%d',)")
-# Calcuate one year ago from the latest date
+    # Calcuate one year ago from the latest date
     year_ago = date_object - dt.timedelta(days=365)
-    temp_obs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > year_ago).all()
+    # Createing a query to retrieve
+    temp_obs_querry = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > year_ago).all()
     
+    # Converting the query results to a Dictionary
+    temp_obs = []
+    for obs in temp_obs_querry:
+        temp_obs_dict = {}
+        temp_obs_dict["date"] = obs.date
+        temp_obs_dict["temp_bservation"] = obs.tobs
+        temp_obs.append(temp_obs_dict)
     return jsonify(temp_obs)
 
-@app.route("/api/v1.0/<start>")
-def start(start):
-    def calc_temps(start_date, end_date):
-    # """TMIN, TAVG, and TMAX for a list of dates.
-    # Args:
-    #     start_date (string): A date string in the format %Y-%m-%d
-    #     end_date (string): A date string in the format %Y-%m-%d       
-    # Returns:
-    #     TMIN, TAVE, and TMAX
-    # """
-        return jsonify(session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-            filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all())
 
-    # latest_date = session.query(Measurement.date).first()
-    output = calc_temps(start, "2017-08-23")
-    return output    
-    # year_ago = date_object - dt.timedelta(days=365)
-    # temp_obs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > year_ago).all()
+@app.route("/api/v1.0/<start>")
+def start_func(start):
+    # Createing a query to retrieve max, min and avg temp observations for a givan date range
+    start_temp_val = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+        .filter(Measurement.date >= start).all()
+    start_date_temp = []
     
+    # Converting the query results to a Dictionary
+    for value in start_temp_val:
+        start_date_temp_dict = {}
+        start_date_temp_dict["min_temp"] = value[0]
+        start_date_temp_dict["avg_temp"] = value[1]
+        start_date_temp_dict["max_temp"] = value[2]
+        start_date_temp.append(start_date_temp_dict)
+    return jsonify(start_date_temp)
+  
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start,end):
-    def calc_temps(start_date, end_date):
-    # """TMIN, TAVG, and TMAX for a list of dates.
-    # Args:
-    #     start_date (string): A date string in the format %Y-%m-%d
-    #     end_date (string): A date string in the format %Y-%m-%d       
-    # Returns:
-    #     TMIN, TAVE, and TMAX
-    # """
-        return jsonify(session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-            filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all())
-
-    # latest_date = session.query(Measurement.date).first()
-    output = calc_temps(start, end)
-    return output   
-
-
-
-
-
-# @app.route("/api/v1.0/names")
-# def names():
-#     """Return a list of all passenger names"""
-#     # Query all passengers
-#     results = session.query(Passenger.name).all()
-
-#     # Convert list of tuples into normal list
-#     all_names = list(np.ravel(results))
-
-#     return jsonify(all_names)
-
-
-# @app.route("/api/v1.0/passengers")
-# def passengers():
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger).all()
-
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for passenger in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = passenger.name
-#         passenger_dict["age"] = passenger.age
-#         passenger_dict["sex"] = passenger.sex
-#         all_passengers.append(passenger_dict)
-
-#     return jsonify(all_passengers)
+    # Createing a query to retrieve max, min and avg temp observations for a givan date range
+    start_end_temp_val = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+            filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    
+    # Converting the query results to a Dictionary 
+    start_end_date_temp = []
+    for val in start_end_temp_val:
+        start_end_date_temp_dict = {}
+        start_end_date_temp_dict["min_temp"] = val[0]
+        start_end_date_temp_dict["avg_temp"] = val[1]
+        start_end_date_temp_dict["max_temp"] = val[2]
+        start_end_date_temp.append(start_end_date_temp_dict)
+    return jsonify(start_end_date_temp)
 
 
 if __name__ == '__main__':
